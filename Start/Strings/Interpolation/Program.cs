283038311ff33b2692e5308d18d86d2a697d2c56
shreyaa12 <shreyagokani31@gmail.com>
2023-02-20 17:38:07 -0700

    learning ph1

--- Start/Strings/Interpolation/Program.cs
+++ Start/Strings/Interpolation/Program.cs
@@ -11,10 +11,11 @@ string c = "CSharp";
 Console.WriteLine("The values are {0}, {1} and {2}", a, b, c);
 
 // TODO: Using string interpolation, the code is much easier to read
-
+Console.WriteLine($"The values are {a},{b},{c}");
 
 // TODO: Interpolated strings can contain expressions as well
-
+Console.WriteLine($"(a+b)/b is {(a+b)/b}");
 
 // TODO: Complex objects can be embedded in strings this way as well
-
+DateTime now = DateTime.Now;
+Console.WriteLine($"Today is {now}");
